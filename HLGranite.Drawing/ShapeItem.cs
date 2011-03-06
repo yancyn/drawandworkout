abd360eca8bbd5488580ef69ef28902273d5488d
yancyn <yancyn@863e2066-8fcb-3358-300e-982895918652>
2011-03-06 12:44:32 +0000

    modifying uml-xsd-class.

---
+++ HLGranite.Drawing/ShapeItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Shapes;
+
+namespace HLGranite.Drawing
+{
+    public partial class ShapeItem: Shape
+    {
+        protected override System.Windows.Media.Geometry DefiningGeometry
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
