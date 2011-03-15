261dafa4c5e49f7c802aaf633ee320e25ea0604a
yancyn <yancyn@863e2066-8fcb-3358-300e-982895918652>
2011-03-15 07:51:54 +0000

    adding cross button on tabitem.

---
+++ trunk/WorkOrderGUI/UserControls/ShapesToolbar.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WorkOrderGUI
+{
+    /// <summary>
+    /// Interaction logic for ShapesToolbar.xaml
+    /// </summary>
+    public partial class ShapesToolbar : UserControl
+    {
+        public ShapesToolbar()
+        {
+            InitializeComponent();
+        }
+    }
+}
