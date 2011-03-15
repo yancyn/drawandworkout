c5ce0c27da90946ae2987dc01171996df83fcf5d
yancyn <yancyn@863e2066-8fcb-3358-300e-982895918652>
2011-03-15 14:51:30 +0000

    - rename CustomControls. - adding icon for menu item. - adding new icon as well.

--- WorkOrderGUI/MainWindow.xaml.cs
+++ WorkOrderGUI/MainWindow.xaml.cs
@@ -17,6 +17,11 @@ namespace WorkOrderGUI
     /// <summary>
     /// Interaction logic for Window1.xaml
     /// </summary>
+    /// <remarks>
+    /// todo: still buggy for popup multiple toolbar.
+    /// TODO: change the selected shape toolbar if there are muliple choices.
+    /// todo: toolbox imcompatible on different screen resolution.
+    /// </remarks>
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -25,16 +30,51 @@ namespace WorkOrderGUI
             //System.Diagnostics.Debug.WriteLine(rect1.Height);
             //rect1.Margin;
             //propertyGrid1.Instance = 
+            //MainMenu.ItemsSource = BuildMenu();
         }
 
-        private void menuClose_Click(object sender, RoutedEventArgs e)
+        private void MenuClose_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
         }
-
         private void Path_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
 
+        }
+        /// <summary>
+        /// Obsolete. Build up menu for application.
+        /// </summary>
+        /// <returns></returns>
+        private List<MenuItem> BuildMenu()
+        {
+            List<MenuItem> items = new List<MenuItem>();
+
+            MenuItem item1 = new MenuItem { Name = "_File" };
+            item1.Items.Add(new MenuItem { Name = "_New" });
+            item1.Items.Add(new MenuItem { Name = "_Open" });
+            item1.Items.Add(new MenuItem { Name = "_Save" });
+            item1.Items.Add(new Separator());
+            item1.Items.Add(new MenuItem { Name = "_Close" });
+
+            MenuItem item2 = new MenuItem { Name = "_Edit" };
+            //item.Command = "";
+            item2.Items.Add(new MenuItem { Name = "_Cut" });
+            item2.Items.Add(new MenuItem { Name = "_Copy" });
+            item2.Items.Add(new MenuItem { Name = "_Paste" });
+
+            MenuItem item3 = new MenuItem { Name = "_Help" };
+            item3.Items.Add(new MenuItem { Name = "_Index" });
+            item3.Items.Add(new MenuItem { Name = "_About" });
+
+            items.Add(item1);
+            items.Add(item2);
+            items.Add(item3);
+            return items;
+        }
+        private void MenuAbout_Click(object sender, RoutedEventArgs e)
+        {
+            string version = "ver " + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            MessageBox.Show("yancyn@gmail.com" + "\n" + version);
         }
     }
 }
