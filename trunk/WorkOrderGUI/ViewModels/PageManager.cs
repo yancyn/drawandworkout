0f3d9d66a2027782277e8efdb06471342acb735d
yancyn <yancyn@863e2066-8fcb-3358-300e-982895918652>
2011-03-16 11:15:09 +0000

    - moved PageTemplate to resource dictionary. - introduce DataTemplateSelector for different type of PageViewModel. - move readme to root folder.

---
+++ trunk/WorkOrderGUI/ViewModels/PageManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace WorkOrderGUI
+{
+    public class PageManager
+    {
+        private PageViewModel currentPage;
+        public PageViewModel CurrentPage { get { return this.currentPage; } }
+        private ObservableCollection<PageViewModel> items;
+        public ObservableCollection<PageViewModel> Items { get { return this.items; } set { this.items = value; } }
+
+        public PageManager()
+        {
+            this.items = new ObservableCollection<PageViewModel>();
+        }
+        public void Add(PageViewModel item)
+        {
+            if (!this.items.Contains(item)) this.items.Add(item);
+            this.currentPage = item;
+        }
+    }
+}
---
+++ trunk/WorkOrderGUI/ViewModels/PageViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using HLGranite.Drawing;
+
+namespace WorkOrderGUI
+{
+    public class PageViewModel
+    {
+        private Object item;
+        public Object Item { get { return this.item; } set { this.item = value; } }
+
+        public PageViewModel(Project project)
+        {
+            this.item = project;
+        }
+        public PageViewModel(DatabaseObject database)
+        {
+            this.item = database;
+        }
+        /// <summary>
+        /// Returns a default key represent this PageViewModel.
+        /// Normally is refer to it's primary key or Guid.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            if (this.item.GetType() == typeof(Project))
+                return (item as Project).Guid.ToString();
+            if (this.item.GetType() == typeof(DatabaseObject))
+            {
+                //todo: return (item as DatabaseObject).Name
+            }
+
+            return base.ToString();
+        }
+    }
+}
