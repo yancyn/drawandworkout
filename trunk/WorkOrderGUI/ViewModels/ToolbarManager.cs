c33d3d117bc0b0246b23a7d3252e0b82d9644e49
yancyn <yancyn@863e2066-8fcb-3358-300e-982895918652>
2011-03-17 03:03:24 +0000

    - viewmodel object must implement System.ComponentModel.INotifyPropertyChanged if not application will not close completely. - try to extract toolbar icon collection into a xaml file and runtime read into GUI by implement MVVM.

---
+++ trunk/WorkOrderGUI/ViewModels/ToolbarManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Xml;
+using System.Windows;
+using System.Windows.Shapes;
+using System.Windows.Markup;
+using System.Windows.Controls;
+
+namespace WorkOrderGUI
+{
+    public class ToolbarManager
+    {
+        private ObservableCollection<ToolbarViewModel> items;
+        public ObservableCollection<ToolbarViewModel> Items { get { return this.items; } }
+        public ToolbarManager()
+        {
+            this.items = new ObservableCollection<ToolbarViewModel>();
+
+            try
+            {
+                StreamReader reader = new StreamReader("Toolbars.xaml");
+                DependencyObject obj = XamlReader.Load(reader.BaseStream) as DependencyObject;
+                Panel panel = ((System.Windows.Controls.Panel)((obj as Window).Content));
+                for (int i = 0; i < panel.Children.Count; i++)
+                {
+                    if (panel.Children[i] is Shape)
+                    {
+                        ToolbarViewModel viewModel = new ToolbarViewModel(panel.Children[i] as Shape);
+                        this.items.Add(viewModel);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex);
+                return;
+            }
+        }
+    }
+}
---
+++ trunk/WorkOrderGUI/ViewModels/ToolbarViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Shapes;
+
+namespace WorkOrderGUI
+{
+    public class ToolbarViewModel : object, System.ComponentModel.INotifyPropertyChanged
+    {
+        private Shape item;
+        public Shape Item { get { return this.item; } set { this.item = value; } }
+        public ToolbarViewModel(Shape shape)
+        {
+            this.item = shape;
+        }
+
+        public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            System.ComponentModel.PropertyChangedEventHandler propertyChanged = this.PropertyChanged;
+            if ((propertyChanged != null))
+            {
+                propertyChanged(this, new System.ComponentModel.PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
