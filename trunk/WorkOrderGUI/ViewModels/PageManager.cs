6a5bf301c5e2b2fcea271065f491c8d17da6d5e4
yancyn <yancyn@863e2066-8fcb-3358-300e-982895918652>
2011-03-19 02:11:40 +0000

    handle open an existing page in tab control.

--- trunk/WorkOrderGUI/ViewModels/PageManager.cs
+++ trunk/WorkOrderGUI/ViewModels/PageManager.cs
@@ -11,17 +11,41 @@ namespace WorkOrderGUI
     /// <summary>
     /// Page collection manager for Tab Control use.
     /// </summary>
-    public class PageManager
+    public class PageManager : System.ComponentModel.INotifyPropertyChanged
     {
         #region Properties
         private PageViewModel currentPage;
-        public PageViewModel CurrentPage { get { return this.currentPage; } }
+        /// <summary>
+        /// Gets or sets current selected page.
+        /// </summary>
+        public PageViewModel CurrentPage { get { return this.currentPage; } set { this.currentPage = value; } }
         private ObservableCollection<PageViewModel> items;
-        public ObservableCollection<PageViewModel> Items { get { return this.items; } set { this.items = value; } }
+        public ObservableCollection<PageViewModel> Items
+        {
+            get { return this.items; }
+            set
+            {
+                if (this.items != null)
+                {
+                    if (this.items.Equals(value) != true)
+                    {
+                        this.items = value;
+                        this.OnPropertyChanged("Items");
+                    }
+                }
+                else
+                {
+                    this.items = value;
+                    this.OnPropertyChanged("Items");
+                }
+            }
+        }
         private NewProject newProject;
         public NewProject NewProject { get { return this.newProject; } }
         private RemovePage removePage;
         public RemovePage RemovePage { get { return this.removePage; } }
+        private OpenInventory openInventory;
+        public OpenInventory OpenInventory { get { return this.openInventory; } }
         #endregion
 
         public PageManager()
@@ -29,19 +53,54 @@ namespace WorkOrderGUI
             this.items = new ObservableCollection<PageViewModel>();
             this.newProject = new NewProject(this);
             this.removePage = new RemovePage(this);
+            this.openInventory = new OpenInventory(this);
         }
 
         #region Methods
         public void Add(PageViewModel item)
         {
-            if (!this.items.Contains(item)) this.items.Add(item);
-            this.currentPage = item;
+            if (item.Item is DatabaseObject)
+            {
+                bool found = false;
+                foreach (PageViewModel viewModel in this.Items)
+                {
+                    if (item.Item.GetType() == viewModel.Item.GetType())
+                    {
+                        found = true;
+                        this.currentPage = item;
+                    }
+                }
+
+                if (!found)
+                {
+                    this.items.Add(item);
+                    this.currentPage = item;
+                }
+            }
+            else if (this.items.Contains(item))//todo: overwrite Contains with key checking on Guid instead of whole memory stack
+            {
+                this.currentPage = item;
+            }
+            else
+            {
+                this.items.Add(item);
+                this.currentPage = item;
+            }
         }
         public void Remove(PageViewModel item)
         {
             if (this.items.Contains(item))
                 this.items.Remove(item);
             if (this.items.Count > 0) this.currentPage = this.items[this.items.Count - 1];
+        }
+        public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
+        public virtual void OnPropertyChanged(string propertyName)
+        {
+            System.ComponentModel.PropertyChangedEventHandler handler = this.PropertyChanged;
+            if ((handler != null))
+            {
+                handler(this, new System.ComponentModel.PropertyChangedEventArgs(propertyName));
+            }
         }
         #endregion
     }
@@ -96,6 +155,29 @@ namespace WorkOrderGUI
             throw new NotImplementedException();
         }
 
+        #endregion
+    }
+    /// <summary>
+    /// Open inventory page if not exist otherwise bring it to focus.
+    /// </summary>
+    public class OpenInventory : ICommand
+    {
+        private PageManager pageManager;
+        public OpenInventory(PageManager sender)
+        {
+            this.pageManager = sender;
+        }
+        #region ICommand Members
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+        public event EventHandler CanExecuteChanged;
+        public void Execute(object parameter)
+        {
+            PageViewModel viewModel = new PageViewModel(new Stocks());
+            this.pageManager.Add(viewModel);
+        }
         #endregion
     }
 }
