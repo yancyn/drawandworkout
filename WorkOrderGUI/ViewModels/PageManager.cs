0a92319f1bc3d262710ada5cc72a5c79e6e1ed94
yancyn <yancyn@863e2066-8fcb-3358-300e-982895918652>
2011-03-21 13:21:41 +0000

    - able to selected tab item in tabcontrol. - display a list under window menu as well and able to switch tab within application.

--- WorkOrderGUI/ViewModels/PageManager.cs
+++ WorkOrderGUI/ViewModels/PageManager.cs
@@ -18,7 +18,44 @@ namespace WorkOrderGUI
         /// <summary>
         /// Gets or sets current selected page.
         /// </summary>
-        public PageViewModel CurrentPage { get { return this.currentPage; } set { this.currentPage = value; } }
+        public PageViewModel CurrentPage
+        {
+            get { return this.currentPage; }
+            set
+            {
+                if (this.currentPage != null)
+                {
+                    if (this.currentPage.Equals(value) != true)
+                    {
+                        this.currentPage = value;
+                        this.OnPropertyChanged("CurrentPage");
+                    }
+                }
+                else
+                {
+                    this.currentPage = value;
+                    this.OnPropertyChanged("CurrentPage");
+                }
+            }
+        }
+        private int currentPageIndex;
+        /// <summary>
+        /// Gets or sets current page index.
+        /// </summary>
+        public int CurrentPageIndex
+        {
+            get
+            {
+                this.currentPageIndex = this.items.IndexOf(this.currentPage);
+                return this.currentPageIndex;
+            }
+            set
+            {
+                this.currentPageIndex = value;
+                this.currentPage = this.items[value];
+                this.OnPropertyChanged("CurrentPageIndex");
+            }
+        }
         private ObservableCollection<PageViewModel> items;
         public ObservableCollection<PageViewModel> Items
         {
@@ -40,12 +77,17 @@ namespace WorkOrderGUI
                 }
             }
         }
+        #endregion
+
+        #region ICommand
         private NewProject newProject;
         public NewProject NewProject { get { return this.newProject; } }
         private RemovePage removePage;
         public RemovePage RemovePage { get { return this.removePage; } }
         private OpenInventory openInventory;
         public OpenInventory OpenInventory { get { return this.openInventory; } }
+        private SelectPage selectPage;
+        public SelectPage SelectPage { get { return this.selectPage; } }
         #endregion
 
         public PageManager()
@@ -54,44 +96,154 @@ namespace WorkOrderGUI
             this.newProject = new NewProject(this);
             this.removePage = new RemovePage(this);
             this.openInventory = new OpenInventory(this);
+            this.selectPage = new SelectPage(this);
         }
 
         #region Methods
         public void Add(PageViewModel item)
         {
+            /*bool found = false;
+            foreach (PageViewModel viewModel in this.items)
+            {
+                if (viewModel.Item == null)
+                {
+                    viewModel.IsSelected = false;
+                    continue;
+                }
+
+                if (viewModel.Item.GetType() == typeof(Project))
+                {
+                    //todo: overwrite Contains with key checking on Guid instead of whole memory stack
+                    if ((viewModel.Item as Project).Guid.ToString()
+                        .Equals((item.Item as Project).Guid.ToString()))
+                    {
+                        found = true;
+                        viewModel.IsSelected = true;
+                        this.currentPage = viewModel;
+                    }
+                    else
+                        viewModel.IsSelected = false;
+                }
+                else if (viewModel.Item.GetType() == item.Item.GetType())
+                {
+                    found = true;
+                    viewModel.IsSelected = true;
+                    this.currentPage = viewModel;
+                }
+                else
+                    viewModel.IsSelected = false;
+            }
+
+            if (!found)
+            {
+                this.items.Add(item);
+                this.items[this.items.Count - 1].IsSelected = true;
+                this.currentPage = item;
+            }*/
+
+
+            
             if (item.Item is DatabaseObject)
             {
                 bool found = false;
                 foreach (PageViewModel viewModel in this.Items)
                 {
+                    if (viewModel.Item == null)
+                    {
+                        viewModel.IsSelected = false;
+                        continue;
+                    }
+
                     if (item.Item.GetType() == viewModel.Item.GetType())
                     {
                         found = true;
+                        viewModel.IsSelected = true;
                         this.currentPage = item;
                     }
+                    else
+                        viewModel.IsSelected = false;
                 }
 
                 if (!found)
                 {
                     this.items.Add(item);
-                    this.currentPage = item;
+                    this.currentPage = this.items[this.items.Count - 1];
+                    this.items[this.items.Count - 1].IsSelected = true;
                 }
             }
-            else if (this.items.Contains(item))//todo: overwrite Contains with key checking on Guid instead of whole memory stack
+            else if (this.items.Contains(item))
+            //else if(item.Item.GetType() == typeof(Project))
             {
                 this.currentPage = item;
+                foreach (PageViewModel viewModel in this.items)
+                    viewModel.IsSelected = false;
+                int index = this.items.IndexOf(item);
+                if (index > -1) this.items[index].IsSelected = true;
             }
             else
             {
                 this.items.Add(item);
                 this.currentPage = item;
+                this.items[this.items.Count - 1].IsSelected = true;
             }
+            
+
+            System.Diagnostics.Debug.WriteLine("currentPage: " + this.currentPage.ToString());
         }
         public void Remove(PageViewModel item)
         {
             if (this.items.Contains(item))
                 this.items.Remove(item);
             if (this.items.Count > 0) this.currentPage = this.items[this.items.Count - 1];
+        }
+        public void Select(PageViewModel item)
+        {
+            if (item.Item is DatabaseObject)
+            {
+                //bool found = false;
+                foreach (PageViewModel viewModel in this.Items)
+                {
+                    if (viewModel.Item == null)
+                    {
+                        viewModel.IsSelected = false;
+                        continue;
+                    }
+
+                    if (item.Item.GetType() == viewModel.Item.GetType())
+                    {
+                        //found = true;
+                        viewModel.IsSelected = true;
+                        this.currentPage = item;
+                    }
+                    else
+                        viewModel.IsSelected = false;
+                }
+
+                //if (!found)
+                //{
+                //    this.items.Add(item);
+                //    this.currentPage = this.items[this.items.Count - 1];
+                //    this.items[this.items.Count - 1].IsSelected = true;
+                //}
+            }
+            else if (this.items.Contains(item))
+            //else if(item.Item.GetType() == typeof(Project))
+            {
+                this.currentPage = item;
+                foreach (PageViewModel viewModel in this.items)
+                    viewModel.IsSelected = false;
+                int index = this.items.IndexOf(item);
+                if (index > -1) this.items[index].IsSelected = true;
+            }
+            //else
+            //{
+            //    this.items.Add(item);
+            //    this.currentPage = item;
+            //    this.items[this.items.Count - 1].IsSelected = true;
+            //}
+
+
+            System.Diagnostics.Debug.WriteLine("currentPage: " + this.currentPage.ToString());
         }
         public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
         public virtual void OnPropertyChanged(string propertyName)
@@ -180,11 +332,40 @@ namespace WorkOrderGUI
         {
             Stocks stocks = new Stocks();
             stocks = DatabaseObject.LoadFromFile() as Stocks;
-            stocks.Refresh();
+            if (stocks == null)
+                stocks = new Stocks();
+            else
+                stocks.Refresh();
 
             PageViewModel viewModel = new PageViewModel(stocks);
             this.pageManager.Add(viewModel);
         }
+        #endregion
+    }
+    public class SelectPage : ICommand
+    {
+        private PageManager manager;
+        public SelectPage(PageManager sender)
+        {
+            this.manager = sender;
+        }
+        #region ICommand Members
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+        public event EventHandler CanExecuteChanged;
+        public void Execute(object parameter)
+        {
+            if (parameter is PageViewModel)
+            {
+                this.manager.Select(parameter as PageViewModel);
+                return;
+            }
+
+            throw new NotImplementedException();
+        }
+
         #endregion
     }
 }
