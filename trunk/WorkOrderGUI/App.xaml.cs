20252f22adb9ddc317e3540f77eca5bcae7b91fa
yancyn <yancyn@863e2066-8fcb-3358-300e-982895918652>
2011-03-22 06:04:48 +0000

    - move all data template to a separate file so that more easier to maintain. - customized own date time format (long & short).

--- trunk/WorkOrderGUI/App.xaml.cs
+++ trunk/WorkOrderGUI/App.xaml.cs
@@ -4,6 +4,8 @@ using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Windows;
+using System.Threading;
+using System.Globalization;
 
 namespace WorkOrderGUI
 {
@@ -12,5 +14,15 @@ namespace WorkOrderGUI
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            //define own date format for application instead of rely on Windows environment.
+            CultureInfo ci = new CultureInfo(Thread.CurrentThread.CurrentCulture.Name);
+            ci.DateTimeFormat.LongDatePattern = "dd/MM/yyyy HH:mm";
+            ci.DateTimeFormat.ShortDatePattern = "dd/MM/yyyy";
+            Thread.CurrentThread.CurrentCulture = ci;
+        }
     }
 }
