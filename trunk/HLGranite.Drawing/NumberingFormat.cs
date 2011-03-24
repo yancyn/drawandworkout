2365b3648faf4dee2f43392d73d3a4cc75ca5ebb
yancyn <yancyn@863e2066-8fcb-3358-300e-982895918652>
2011-03-24 03:40:08 +0000

    get binding the config value into screen and able to save out.

---
+++ trunk/HLGranite.Drawing/NumberingFormat.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HLGranite.Drawing
+{
+    public enum NumberingFormat
+    {
+        /// <summary>
+        /// 1, 2, 3, 4...
+        /// </summary>
+        Digit,
+        /// <summary>
+        /// I, II, III, IV, ...
+        /// </summary>
+        Roman,
+        /// <summary>
+        /// A, B, C, D, ...
+        /// </summary>
+        Alphabet,
+        /// <summary>
+        /// 甲, 乙, 丙, 丁, ...
+        /// </summary>
+        Chinese,
+    }
+}
