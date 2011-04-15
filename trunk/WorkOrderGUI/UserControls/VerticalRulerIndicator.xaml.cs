4d78eb334e5e7c65978c4dc596c7e8824687ccd6
yancyn <yancyn@863e2066-8fcb-3358-300e-982895918652>
2011-04-15 06:59:00 +0000

    created user control for ruler. this is useful and can be easily setup during creating all shape template.

---
+++ trunk/WorkOrderGUI/UserControls/VerticalRulerIndicator.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WorkOrderGUI
+{
+    /// <summary>
+    /// Interaction logic for MeasurementIndicator.xaml
+    /// </summary>
+    public partial class VerticalRulerIndicator : UserControl
+    {
+        #region Properties
+        /// <summary>
+        /// Gets or sets the length of indicator line.
+        /// </summary>
+        /// <remarks>
+        /// If Orientation is Horizontal then width = length.
+        /// If Orientation is Vertical then height = length.
+        /// </remarks>
+        public double Length
+        {
+            get { return (double)GetValue(LengthProperty); }
+            set { SetValue(LengthProperty, value); }
+        }
+        public static readonly DependencyProperty LengthProperty = DependencyProperty.Register(
+            "Length",
+            typeof(double),
+            typeof(VerticalRulerIndicator));
+        /// <summary>
+        /// Gets or sets value to display in ruler (integer, decimal, or double).
+        /// </summary>
+        public object Value
+        {
+            get { return (object)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(
+            "Value",
+            typeof(object),
+            typeof(VerticalRulerIndicator));
+        #endregion
+        public VerticalRulerIndicator()
+        {
+            InitializeComponent();
+        }
+    }
+    /// <summary>
+    /// Gets top margin for arrow.
+    /// </summary>
+    public class ArrowTopMarginConverter : IValueConverter
+    {
+        #region IValueConverter Members
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is double)
+            {
+                double length = System.Convert.ToDouble(value);
+                return length - 10;
+            }
+            else if (value is decimal)
+            {
+                decimal length = System.Convert.ToDecimal(value);
+                return length - 10;
+            }
+            else if (value is int)
+            {
+                int length = System.Convert.ToInt32(value);
+                return length - 10;
+            }
+
+            throw new NotImplementedException("Not supported type of " + value.GetType());
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+    /// <summary>
+    /// Gets middle point to determine label position.
+    /// </summary>
+    public class VerticalMiddlePointConverter : IValueConverter
+    {
+        #region IValueConverter Members
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is double)
+            {
+                double length = System.Convert.ToDouble(value);
+                return length / 2;
+            }
+            else if (value is decimal)
+            {
+                decimal length = System.Convert.ToDecimal(value);
+                return length / 2;
+            }
+            else if (value is int)
+            {
+                int length = System.Convert.ToInt32(value);
+                return length / 2;
+            }
+
+            throw new NotImplementedException("Not supported type of " + value.GetType());
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
